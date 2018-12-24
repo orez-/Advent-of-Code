3d4d0c62370f9c6c0d14dcda1800992056b9ca26
Brian Shaginaw <brian.shaginaw@warbyparker.com>
2018-12-24 01:15:15 -0500

    Fixup day 24: Immune System Simulator 20XX.

--- day24/original.py
+++ day24/original.py
@@ -1,6 +1,4 @@
-import collections
 import itertools
-import hashlib
 import re
 
 
@@ -13,7 +11,7 @@ class Squad:
         self.dmg = dmg
         self.atk_type = atk_type
         self.initiative = initiative
-        self.team = team
+        self.team = team  # only for display
 
     @property
     def effective_power(self):
@@ -32,14 +30,20 @@ class Squad:
         self.units -= units
         return (self.units <= 0, units)
 
+    def targeting_order(self):
+        """
+        Value ranking what order this unit should pick a target relative to other squads.
+
+        Higher values are higher priority.
+        """
+        return (self.effective_power, self.initiative)
+
     def target_score(self, target):
+        """
+        Value ranking the potential of the target as a target. The highest-scored squad is chosen
+        as a target.
+        """
         return (self.attack_damage(target), target.effective_power, target.initiative)
-
-    def __eq__(self, other):
-        return self.initiative == other.initiative
-
-    def __hash__(self):
-        return hash(self.initiative)
 
     def __repr__(self):
         return f"{self.team}({self.initiative}, units={self.units})"
@@ -63,14 +67,11 @@ def get_squads(file, team, bonus=0):
         yield Squad(units, hp, weak, immune, dmg + bonus, atk_type, initiative, team)
 
 
-def part1(immune_sys_file, infect_sys_file):
-    immune_sys = set(get_squads(immune_sys_file, 'Immune'))
-    infect_sys = set(get_squads(infect_sys_file, 'Infect'))
-
+def fight(immune_sys, infect_sys):
     while True:
-        immune_priority = sorted(immune_sys, key=lambda im: (im.effective_power, im.initiative), reverse=True)
+        immune_priority = sorted(immune_sys, key=Squad.targeting_order, reverse=True)
         imm_targets = set(immune_sys)
-        infect_priority = sorted(infect_sys, key=lambda im: (im.effective_power, im.initiative), reverse=True)
+        infect_priority = sorted(infect_sys, key=Squad.targeting_order, reverse=True)
         inf_targets = set(infect_sys)
 
         # Target selection
@@ -99,74 +100,39 @@ def part1(immune_sys_file, infect_sys_file):
 
         # Attack
         targets = sorted(targets, key=lambda elem: elem[0].initiative, reverse=True)
+        did_damage = False
         for atkr, defr in targets:
+            # Don't act if dead
             if atkr not in immune_sys and atkr not in infect_sys:
                 continue
             dmg = atkr.attack_damage(defr)
-            if defr.hit(dmg)[0]:  # killed
+            killed, lost = defr.hit(dmg)
+            if killed:  # killed
                 immune_sys.discard(defr)
                 infect_sys.discard(defr)
+            if lost:
+                did_damage = True
 
-        if not immune_sys or not infect_sys:
-            return sum(i.units for i in immune_sys | infect_sys)
+        if not did_damage or not immune_sys or not infect_sys:
+            return immune_sys, infect_sys
+
+
+def part1(immune_sys_file, infect_sys_file):
+    immune_sys = set(get_squads(immune_sys_file, 'Immune'))
+    infect_sys = set(get_squads(infect_sys_file, 'Infect'))
+
+    immune_sys, infect_sys = fight(immune_sys, infect_sys)
+    return sum(i.units for i in immune_sys | infect_sys)
 
 
 def part2(immune_sys_file, infect_sys_file):
-    for bonus in itertools.count(187):
+    for bonus in itertools.count(1):
         immune_sys = set(get_squads(immune_sys_file, 'Immune', bonus=bonus))
         infect_sys = set(get_squads(infect_sys_file, 'Infect'))
 
-        while True:
-            immune_priority = sorted(immune_sys, key=lambda im: (im.effective_power, im.initiative), reverse=True)
-            imm_targets = set(immune_sys)
-            infect_priority = sorted(infect_sys, key=lambda im: (im.effective_power, im.initiative), reverse=True)
-            inf_targets = set(infect_sys)
-
-            # Target selection
-            targets = []
-            for imm in immune_priority:
-                try:
-                    inf_trg = max((
-                        inf for inf in inf_targets
-                        if imm.attack_damage(inf)
-                    ), key=lambda inf: imm.target_score(inf))
-                except ValueError:
-                    continue
-                targets.append((imm, inf_trg))
-                inf_targets.discard(inf_trg)
-
-            for inf in infect_priority:
-                try:
-                    imm_trg = max((
-                        imm for imm in imm_targets
-                        if inf.attack_damage(imm)
-                    ), key=lambda imm: inf.target_score(imm))
-                except ValueError:
-                    continue
-                targets.append((inf, imm_trg))
-                imm_targets.discard(imm_trg)
-
-            # Attack
-            targets = sorted(targets, key=lambda elem: elem[0].initiative, reverse=True)
-            did_damage = False
-            for atkr, defr in targets:
-                if atkr not in immune_sys and atkr not in infect_sys:
-                    continue
-                dmg = atkr.attack_damage(defr)
-                killed, lost = defr.hit(dmg)
-                if killed:  # killed
-                    immune_sys.discard(defr)
-                    infect_sys.discard(defr)
-                if lost:
-                    did_damage = True
-
-            if not did_damage:
-                break
-            if not immune_sys:
-                break
-            if not infect_sys:
-                return sum(i.units for i in immune_sys)
-
+        immune_sys, infect_sys = fight(immune_sys, infect_sys)
+        if not infect_sys:
+            return sum(i.units for i in immune_sys)
 
 
 if __name__ == '__main__':
