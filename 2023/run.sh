72fb84a44de794ff19cf678f9a70f508d3de8400
Brian Shaginaw <bshaginaw120@gmail.com>
2023-12-07 00:27:08 -0500

    Allow specifying test file

--- 2023/run.sh
+++ 2023/run.sh
@@ -1,8 +1,9 @@
 day=$1
 part=$2
+filename="${3:-file.txt}"
 [ -z "$part" ] && {
-  >&2 echo 'usage: ./run.sh dayXX partY'
+  >&2 echo 'usage: ./run.sh dayXX partY [testfile.txt]'
   exit 1
 }
 
-idris2 $day/$part.idr -p contrib -x main < $day/file.txt
+idris2 $day/$part.idr -p contrib -x main < $day/$filename
