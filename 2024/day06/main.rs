0536446e325087ca2ce828439cc7d1e0b2c18cc5
Brian Shaginaw <bshaginaw120@gmail.com>
2024-12-06 00:43:32 -0500

    Change the code a bit
    Clearing the seen set instead of making a new one speeds it up a little, but not a ton.
    Also factored this into a common walk fn: unclear if this is better

--- 2024/day06/main.rs
+++ 2024/day06/main.rs
@@ -2,75 +2,81 @@ use std::collections::HashSet;
 use std::env;
 use std::io::{self, BufRead};
 
-fn part1(lines: Vec<Vec<u8>>) -> usize {
-    let width = lines[0].len();
-    let height = lines.len();
-    let (mut x, mut y) = lines.iter().enumerate().find_map(|(y, line)| {
+type Grid = Vec<Vec<u8>>;
+type Coord = (usize, usize);
+
+fn find_start(grid: &Grid) -> Coord {
+    grid.iter().enumerate().find_map(|(y, line)| {
         let x = line.iter().position(|&elem| elem == b'^')?;
         Some((x, y))
-    }).unwrap();
-    let mut seen = HashSet::new();
+    }).unwrap()
+}
+
+/// Walk the grid starting at `(x, y)` and apply fn `each` at each position.
+/// `each` returns if we should continue walking: if we should stop walking,
+/// `walk` returns `true`. `walk` returns `false` instead if we walk out
+/// of the grid.
+fn walk<F>((mut x, mut y): Coord, grid: &Grid, mut each: F) -> bool
+where
+    F: FnMut(usize, usize, isize, isize) -> bool,
+{
+    let width = grid[0].len();
+    let height = grid.len();
+
     let mut dx = 0isize;
     let mut dy = -1isize;
 
     loop {
-        seen.insert((x, y));
+        if !each(x, y, dx, dy) {
+            return true;
+        }
         let nx = x.wrapping_add_signed(dx);
         let ny = y.wrapping_add_signed(dy);
-        if nx >= width || ny >= height { break }
-        if lines[ny][nx] == b'#' {
+        if nx >= width || ny >= height {
+            break;
+        }
+        if grid[ny][nx] == b'#' {
             (dx, dy) = (-dy, dx);
         } else {
             (x, y) = (nx, ny)
         }
     }
+    false
+}
+
+fn part1(grid: Grid) -> usize {
+    let start = find_start(&grid);
+
+    let mut seen = HashSet::new();
+    walk(start, &grid, |x, y, _, _| {
+        seen.insert((x, y));
+        true
+    });
     seen.len()
 }
 
-fn part2(mut lines: Vec<Vec<u8>>) -> i32 {
-    let width = lines[0].len();
-    let height = lines.len();
-    let (sx, sy) = lines.iter().enumerate().find_map(|(y, line)| {
-        let x = line.iter().position(|&elem| elem == b'^')?;
-        Some((x, y))
-    }).unwrap();
+fn part2(mut grid: Grid) -> usize {
+    let width = grid[0].len();
+    let height = grid.len();
+    let start = find_start(&grid);
 
+    let mut seen = HashSet::new();
     let mut total = 0;
     for ox in 0..width {
         for oy in 0..height {
-            if lines[oy][ox] != b'.' { continue }
-            lines[oy][ox] = b'#';
+            if grid[oy][ox] != b'.' { continue }
+            grid[oy][ox] = b'#';
 
-            let mut seen = HashSet::new();
-            let mut x = sx;
-            let mut y = sy;
-            let mut dx = 0isize;
-            let mut dy = -1isize;
+            seen.clear();
+            total += walk(start, &grid, |x, y, dx, dy| seen.insert((x, y, dx, dy))) as usize;
 
-            loop {
-                if !seen.insert((x, y, dx, dy)) {
-                    total += 1;
-                    break
-                }
-                let nx = x.wrapping_add_signed(dx);
-                let ny = y.wrapping_add_signed(dy);
-                if nx >= width || ny >= height {
-                    break;
-                }
-                if lines[ny][nx] == b'#' {
-                    (dx, dy) = (-dy, dx);
-                } else {
-                    (x, y) = (nx, ny)
-                }
-            };
-
-            lines[oy][ox] = b'.';
+            grid[oy][ox] = b'.';
         }
     }
     total
 }
 
-fn read_lines() -> io::Result<Vec<Vec<u8>>> {
+fn read_lines() -> io::Result<Grid> {
     let stdin = io::stdin();
     stdin.lock().lines().map(|x| Ok(x?.into_bytes())).collect()
 }
