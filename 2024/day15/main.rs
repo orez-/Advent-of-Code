2b173d3977f8831f3113f683f3b95008808a34df
Brian Shaginaw <bshaginaw120@gmail.com>
2024-12-15 01:42:35 -0500

    Add part 2

--- 2024/day15/main.rs
+++ 2024/day15/main.rs
@@ -3,38 +3,80 @@ use std::io::{self, BufRead};
 
 type Direction = (isize, isize);
 
-struct Sokoban {
+#[derive(Clone)]
+struct Sokoban<T> {
     robot: (usize, usize),
-    grid: Vec<Vec<Tile>>,
+    grid: Vec<Vec<T>>,
 }
 
-fn display(sokoban: &Sokoban) {
-    let mut out = String::new();
-    for (y, row) in sokoban.grid.iter().enumerate() {
-        for (x, elem) in row.iter().enumerate() {
-            if (x, y) == sokoban.robot {
-                out.push('@');
-            } else {
-                match elem {
-                    Tile::Block => out.push('O'),
-                    Tile::Floor => out.push('.'),
-                    Tile::Wall => out.push('#'),
+impl<T: Eq> Sokoban<T> {
+    fn count_score(&self, tile: &T) -> usize {
+        let mut total = 0;
+        for (y, row) in self.grid.iter().enumerate() {
+            for (x, elem) in row.iter().enumerate() {
+                if elem == tile {
+                    total += y * 100 + x;
                 }
             }
         }
-        out.push('\n');
+        total
     }
-    println!("{out}");
 }
 
-#[derive(Clone, Copy)]
+impl Sokoban<Tile2> {
+    fn shove(&mut self, (x, y): (usize, usize), dir: Direction) -> bool {
+        match self.grid[y][x] {
+            Tile2::Floor => true,
+            Tile2::Wall => false,
+            Tile2::BlockLeft => {
+                self.grid[y][x] = Tile2::Floor;
+                self.grid[y][x + 1] = Tile2::Floor;
+                let nx = x.wrapping_add_signed(dir.0);
+                let ny = y.wrapping_add_signed(dir.1);
+                let can =
+                    self.shove((nx, ny), dir) &&
+                    self.shove((nx + 1, ny), dir);
+                self.grid[ny][nx] = Tile2::BlockLeft;
+                self.grid[ny][nx + 1] = Tile2::BlockRight;
+                can
+            }
+            // lazy
+            Tile2::BlockRight => self.shove((x - 1, y), dir),
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
 enum Tile {
     Block,
     Floor,
     Wall,
 }
 
-fn part1((mut sokoban, directions): (Sokoban, Vec<Direction>)) -> usize {
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum Tile2 {
+    BlockLeft,
+    BlockRight,
+    Floor,
+    Wall,
+}
+
+fn part1((grid, directions): (Vec<String>, Vec<Direction>)) -> usize {
+    let mut robot = None;
+    let grid = grid.iter().enumerate().map(|(y, row)| {
+        row.chars().enumerate().map(|(x, elem)| match elem {
+            '@' => {
+                robot = Some((x, y));
+                Tile::Floor
+            }
+            '.' => Tile::Floor,
+            '#' => Tile::Wall,
+            'O' => Tile::Block,
+            c => panic!("dunno what a '{c}' is"),
+        }).collect()
+    }).collect();
+    let mut sokoban = Sokoban { grid, robot: robot.unwrap() };
+
     for (dx, dy) in directions {
         let (x, y) = sokoban.robot;
         let rx = x.wrapping_add_signed(dx);
@@ -54,41 +96,46 @@ fn part1((mut sokoban, directions): (Sokoban, Vec<Direction>)) -> usize {
         sokoban.grid[ny][nx] = lift;
         sokoban.grid[ry][rx] = Tile::Floor;
         sokoban.robot = (rx, ry);
-        display(&sokoban);
     }
 
-    let mut total = 0;
-    for (y, row) in sokoban.grid.iter().enumerate() {
-        for (x, elem) in row.iter().enumerate() {
-            if matches!(elem, Tile::Block) {
-                total += y * 100 + x;
-            }
-        }
-    }
-    total
+    sokoban.count_score(&Tile::Block)
 }
 
-fn part2((mut sokoban, directions): (Sokoban, Vec<Direction>)) -> u64 {
-    todo!();
-}
-
-fn read_lines() -> (Sokoban, Vec<Direction>) {
-    let stdin = io::stdin();
-    let mut lines = stdin.lock().lines().map(|line| line.unwrap());
+fn part2((grid, directions): (Vec<String>, Vec<Direction>)) -> usize {
     let mut robot = None;
-    let grid = lines.by_ref().take_while(|line| !line.is_empty()).enumerate().map(|(y, row)| {
-        row.chars().enumerate().map(|(x, elem)| match elem {
+    let grid = grid.iter().enumerate().map(|(y, row)| {
+        row.chars().enumerate().flat_map(|(x, elem)| match elem {
             '@' => {
-                robot = Some((x, y));
-                Tile::Floor
+                robot = Some((x * 2, y));
+                [Tile2::Floor, Tile2::Floor]
             }
-            '.' => Tile::Floor,
-            '#' => Tile::Wall,
-            'O' => Tile::Block,
+            '.' => [Tile2::Floor, Tile2::Floor],
+            '#' => [Tile2::Wall, Tile2::Wall],
+            'O' => [Tile2::BlockLeft, Tile2::BlockRight],
             c => panic!("dunno what a '{c}' is"),
         }).collect()
     }).collect();
-    let sokoban = Sokoban { grid, robot: robot.unwrap() };
+    let mut sokoban = Sokoban { grid, robot: robot.unwrap() };
+
+    for (dx, dy) in directions {
+        let mut potential = sokoban.clone();
+        let (x, y) = sokoban.robot;
+        let rx = x.wrapping_add_signed(dx);
+        let ry = y.wrapping_add_signed(dy);
+        if potential.shove((rx, ry), (dx, dy)) {
+            sokoban = potential;
+            sokoban.robot = (rx, ry);
+        }
+    }
+
+    sokoban.count_score(&Tile2::BlockLeft)
+}
+
+fn read_lines() -> (Vec<String>, Vec<Direction>) {
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines().map(|line| line.unwrap());
+    let grid = lines.by_ref().take_while(|line| !line.is_empty()).collect();
+
     let directions = lines.flat_map(|line| line.into_bytes()).map(|c| match c {
         b'^' => (0, -1),
         b'>' => (1, 0),
@@ -96,7 +143,7 @@ fn read_lines() -> (Sokoban, Vec<Direction>) {
         b'<' => (-1, 0),
         c => panic!("can't do a {c}"),
     }).collect();
-    (sokoban, directions)
+    (grid, directions)
 }
 
 fn main() -> io::Result<()> {
