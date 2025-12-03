79a8dafe401bd2bd66986e2a223fd174b6465c4a
Brian Shaginaw <bshaginaw120@gmail.com>
2025-12-03 09:43:09 -0500

    Consolidate I guess

--- 2025/day03/main.rs
+++ 2025/day03/main.rs
@@ -2,33 +2,14 @@ use std::env;
 use std::io::{self, BufRead};
 use std::cmp::Reverse;
 
-fn part1(banks: Vec<String>) -> u32 {
-    let mut out = 0;
-    for bank in banks {
-        let mut bank = bank.into_bytes();
-        bank.iter_mut().for_each(|x| *x -= b'0');
-        let init = bank.len() - 1;
-        let (idx, tens) = bank[..init]
-            .iter()
-            .copied()
-            .enumerate()
-            .max_by_key(|&(i, e)| (e, Reverse(i)))
-            .expect("at least one element");
-        let ones = bank[idx+1..].iter().max().expect("at least one element");
-        let val = (tens * 10 + ones) as u32;
-        out += val;
-    }
-    out
-}
-
-fn part2(banks: Vec<String>) -> u64 {
+fn find_batteries(banks: Vec<String>, digits: usize) -> u64 {
     let mut out = 0;
     for bank in banks {
         let bank: Vec<_> = bank.bytes().map(|x| (x - b'0') as u64).collect();
         let mut bank: &[_] = &bank;
 
         let mut num = 0;
-        for remaining in (0..12).rev() {
+        for remaining in (0..digits).rev() {
             let init = bank.len() - remaining;
             let (idx, digit) = bank[..init]
                 .iter()
@@ -52,8 +33,8 @@ fn read_lines() -> io::Result<Vec<String>> {
 
 fn main() -> io::Result<()> {
     match env::args().nth(1).as_deref() {
-        Some("part1") => println!("{}", part1(read_lines()?)),
-        Some("part2") => println!("{}", part2(read_lines()?)),
+        Some("part1") => println!("{}", find_batteries(read_lines()?, 2)),
+        Some("part2") => println!("{}", find_batteries(read_lines()?, 12)),
         Some(word) => eprintln!("Please specify 'part1' or 'part2', not {:?}", word),
         None => eprintln!("Please specify 'part1' or 'part2'"),
     }
